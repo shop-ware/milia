ad1970ea2df0a92e993ceee63e53bf4713a502aa
Ryan Fisher <ryan.fisher@shop-ware.com>
2021-06-11 16:05:43 -0700

    Comment out methods that override devise methods we rely on

--- lib/milia/engine.rb
+++ lib/milia/engine.rb
@@ -3,14 +3,13 @@ require 'milia'
 module Milia
   class Engine < Rails::Engine
     initializer 'milia.include' do
+      ActiveSupport.on_load(:active_record) do
+        include Milia::Base
+      end
 
-        ActiveSupport.on_load(:active_record) do
-          include Milia::Base
-        end
-
-        ActiveSupport.on_load(:action_controller_base) do
-          include Milia::Control
-        end
+      ActiveSupport.on_load(:action_controller_base) do
+        include Milia::Control
+      end
     end
 
     rake_tasks do
